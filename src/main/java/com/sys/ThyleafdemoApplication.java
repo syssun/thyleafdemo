9a55a2aef47152665df1358a4a7b966344db2148
syssun <2506715686@qq.com>
2019-07-09 19:21:27 +0800

    Initial commit

---
+++ src/main/java/com/sys/ThyleafdemoApplication.java
@@ -0,0 +1,13 @@
+package com.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThyleafdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThyleafdemoApplication.class, args);
+    }
+
+}
